544e8af70f42a25ce81642d9eeeb254d358f7c5b
Nick <nickinsanjuan@gmail.com>
2017-04-25 12:30:22 -0700

    Added entity tracking funcitons

--- tracking_test.py
+++ tracking_test.py
@@ -1,29 +1,9 @@
-# ------------------------------------------------------------------------------------------------
-# Copyright (c) 2016 Microsoft Corporation
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-# associated documentation files (the "Software"), to deal in the Software without restriction,
-# including without limitation the rights to use, copy, modify, merge, publish, distribute,
-# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or
-# substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# ------------------------------------------------------------------------------------------------
-
-# Tutorial sample #2: Run simple mission using raw XML
-
 import MalmoPython
 import os
 import sys
 import time
 import json
+import math
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 
@@ -31,8 +11,8 @@ sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immedi
 
 TRACK_WIDTH = 20
 TRACK_BREADTH = 20
-
-missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+PLAYER_NAME = "Killer"
+missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
             <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 
               <About>
@@ -42,11 +22,15 @@ missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
               <ServerSection>
                 <ServerInitialConditions>
                     <AllowSpawning>true</AllowSpawning>
+                    <Time>
+                        <StartTime>6000</StartTime>
+                        <AllowPassageOfTime>false</AllowPassageOfTime>
+                    </Time>
                 </ServerInitialConditions>
                 <ServerHandlers>
                   <FlatWorldGenerator generatorString="3;7,220*1,5*3,2;3;,biome_1"/>
                   <DrawingDecorator>
-                    <DrawEntity x="10.0" y="227.0" z="10" type="Pig"/>
+                    <DrawEntity x="2.0" y="227.0" z="10" type="Pig"/>
                   </DrawingDecorator>
                   <ServerQuitFromTimeUp timeLimitMs="30000"/>
                   <ServerQuitWhenAnyAgentFinishes/>
@@ -55,14 +39,17 @@ missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
               </ServerSection>
 
               <AgentSection mode="Survival">
-                <Name>MalmoTutorialBot</Name>
-                <AgentStart/>
+                <Name>''' + PLAYER_NAME + '''</Name>
+                <AgentStart>
+                    <Placement x="0.5" y="227" z="0.5"/>
+                    <Inventory>
+                    </Inventory>
+                </AgentStart>
                 <AgentHandlers>
                   <ObservationFromFullStats/>
-                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                  <ContinuousMovementCommands turnSpeedDegs="360"/>
                   <ObservationFromNearbyEntities>
-                    <Range name="entities" xrange="''' + str(TRACK_WIDTH) + '''" yrange="2" zrange="''' + str(
-    TRACK_BREADTH) + '''" />
+                    <Range name="entities" xrange="'''+str(TRACK_WIDTH)+'''" yrange="2" zrange="'''+str(TRACK_BREADTH)+'''" />
                   </ObservationFromNearbyEntities>
                 </AgentHandlers>
               </AgentSection>
@@ -70,11 +57,23 @@ missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 
 # Create default Malmo objects:
 
+def calcYawToMob(entity, x, z): #Borrowed from cart_test.py
+    ''' Find the mob we are following, and calculate the yaw we need in order to face it '''
+    dx = entity['x'] - x
+    dz = entity['z'] - z
+    yaw = -180 * math.atan2(dx, dz) / math.pi
+    return yaw
+
+def getNextTarget(entities):
+    for entity in entities:
+        if entity['name'] != PLAYER_NAME:
+            return entity
+
 agent_host = MalmoPython.AgentHost()
 try:
-    agent_host.parse(sys.argv)
+    agent_host.parse( sys.argv )
 except RuntimeError as e:
-    print 'ERROR:', e
+    print 'ERROR:',e
     print agent_host.getUsage()
     exit(1)
 if agent_host.receivedArgument("help"):
@@ -88,11 +87,11 @@ my_mission_record = MalmoPython.MissionRecordSpec()
 max_retries = 3
 for retry in range(max_retries):
     try:
-        agent_host.startMission(my_mission, my_mission_record)
+        agent_host.startMission( my_mission, my_mission_record )
         break
     except RuntimeError as e:
         if retry == max_retries - 1:
-            print "Error starting mission:", e
+            print "Error starting mission:",e
             exit(1)
         else:
             time.sleep(2)
@@ -105,7 +104,7 @@ while not world_state.has_mission_begun:
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
-        print "Error:", error.text
+        print "Error:",error.text
 
 print
 print "Mission running ",
@@ -113,13 +112,30 @@ print "Mission running ",
 # Loop until mission ends:
 while world_state.is_mission_running:
     sys.stdout.write(".")
+    print
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     if world_state.number_of_observations_since_last_state > 0:
         msg = world_state.observations[-1].text
         ob = json.loads(msg)
+        xPos = ob['XPos']
+        zPos = ob['ZPos']
+        yaw = ob['Yaw']
+        target = getNextTarget(ob['entities'])
+        yaw_to_mob = calcYawToMob(target, xPos, zPos)
+        deltaYaw = yaw_to_mob - yaw
+        while deltaYaw < -180:
+            deltaYaw += 360;
+        while deltaYaw > 180:
+            deltaYaw -= 360;
+        deltaYaw /= 180.0;
+        # And turn:
+        agent_host.sendCommand("turn " + str(deltaYaw))
+
+
     for error in world_state.errors:
-        print "Error:", error.text
+        print "Error:",error.text
 
 print
 print "Mission ended"
+# Mission has ended.
